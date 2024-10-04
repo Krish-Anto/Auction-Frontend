f2a1af36f0cadb5f13a745bc0ee9ce016666a567
Krish-Anto <krishnarajan1995@gmail.com>
2024-10-04 23:45:38 +0530

    NavbarclassName

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 function Nav() {
   return (
     <div>
-      <nav class="navbar navbar-expand-lg navbar-light bg-primary">
-        <div class="container-fluid">
-          <text class="navbar-brand" >
+      <nav className="navbar navbar-expand-lg navbar-light bg-primary">
+        <div className="container-fluid">
+          <text className="navbar-brand" >
             Lets Adopt Pet
           </text>
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarNavDropdown"
@@ -17,28 +17,28 @@ function Nav() {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
                   Home
                 </a>
               </li>
-              {/* <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Features
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Add Pet
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
+              {/* <li className="nav-item">
+                <a className="nav-link" href="#">
                   Pricing
                 </a>
-              </li>
-              <li class="nav-item dropdown">
+              </li> */}
+              {/* <li className="nav-item dropdown">
                 <a
-                  class="nav-link dropdown-toggle"
+                  className="nav-link dropdown-toggle"
                   href="#"
                   id="navbarDropdownMenuLink"
                   role="button"
@@ -48,21 +48,21 @@ function Nav() {
                   Dropdown link
                 </a>
                 <ul
-                  class="dropdown-menu"
+                  className="dropdown-menu"
                   aria-labelledby="navbarDropdownMenuLink"
                 >
                   <li>
-                    <a class="dropdown-item" href="#">
+                    <a className="dropdown-item" href="#">
                       Action
                     </a>
                   </li>
                   <li>
-                    <a class="dropdown-item" href="#">
+                    <a className="dropdown-item" href="#">
                       Another action
                     </a>
                   </li>
                   <li>
-                    <a class="dropdown-item" href="#">
+                    <a className="dropdown-item" href="#">
                       Something else here
                     </a>
                   </li>
